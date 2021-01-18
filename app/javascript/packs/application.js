050aa06de07fbc2ce3d01abd69c40a6ceaa52394
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-18 18:01:23 +0900

    vueファイル作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { createApp } from "vue";
+import App from "../app.vue"
+
+document.addEventListener('DOMContentLoaded', () => {
+  createApp(App).mount('#app')
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,13 +5,13 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 
-environment.plugins.prepend(
-  'Define',
-  new DefinePlugin({
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false
-  })
-)
+// environment.plugins.prepend(
+//   'Define',
+//   new DefinePlugin({
+//       __VUE_OPTIONS_API__: true,
+//       __VUE_PROD_DEVTOOLS__: false
+//   })
+// )
 
 environment.loaders.prepend('vue', vue)
 

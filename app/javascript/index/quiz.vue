8e6ac29e82364d19d406cb9c42c02d2048c882b1
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-19 22:23:00 +0900

    Update quiz.vue

--- app/javascript/index/quiz.vue
+++ app/javascript/index/quiz.vue
@@ -2,8 +2,19 @@
 <div class="columns is-mobile is-centered">
   <div class="column is-half">
     <h1 class="title is-1 has-text-centered">Vue.jsクイズ</h1>
-    <question></question>
-    <answer></answer>
+    <question
+      v-if="currentQuestion"
+      :question-text="currentQuestion.text"
+      @click-yes-btn="doAnswer($event)"
+      @click-no-btn="doAnswer($event)"
+    ></question>
+    <answer
+      v-if="!currentQuestion"
+      :total-score="totalScore"
+      :questions="questions"
+      :your-answers="yourAnswers"
+      @click="doRestart"
+    ></answer>
   </div>
 </div>
 </template>
@@ -12,10 +23,69 @@
 import Question from './question.vue'
 import Answer from './answer.vue'
 
+const ANSWER = { YES: 1, NO: 0 }
+const MAX_POINT = 100
+
 export default {
   components: {
     'question': Question,
     'answer': Answer,
   },
+  data() {
+    return {
+      questions: [
+        {
+          text: "たまプラーザは駅である",
+          answer: ANSWER.NO
+        },
+        {
+          text: "たまプラーザは街である",
+          answer: ANSWER.YES
+        },
+        {
+          text: "ニドラン♂",
+          answer: ANSWER.NO
+        },
+      ],
+      currentIndex: 0,
+      yourAnswers: [],
+    }
+  },
+  computed: {
+    currentQuestion: function() {
+      return this.questions[this.currentIndex]
+    },
+    correctAnswers: function() {
+      const self = this
+      return this.questions.filter(function(question, index) {
+        return question.answer === self.yourAnswers[index]
+      })
+    },
+    totalScore: function() {
+      const score = MAX_POINT / this.questions.length
+      return Math.floor(score * this.correctAnswers.length)
+    }
+  },
+  methods: {
+    doAnswer: function() {
+      this.yourAnswers[this.currentIndex] = answer
+      this.nextQuestion()
+    },
+    nextQuestion: function() {
+      if (this.currentIndex < this.questions.length) {
+        this.currentIndex += 1
+      }
+    },
+    doRestart: function() {
+      this.currentIndex = 0
+      this.initYourAnswersArray()
+    },
+    initYourAnswersArray: function() {
+      this.yourAnswers = Array(this.questions.length)
+    }
+  },
+  mounted: function() {
+    this.initYourAnswersArray()
+  }
 }
 </script>

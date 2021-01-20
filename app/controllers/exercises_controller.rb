9878a4330f277b861e1a5ab31177687d8d1598ce
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-20 16:08:38 +0900

    アクション定義、ビュー作成

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,15 +1,20 @@
 class ExercisesController < ApplicationController
   def index
-    posts = Tweet.all
+  end
+  
+  def post
+    posts = Post.all
     respond_to do |format|
       format.html
       format.json { render json: posts }
     end
   end
-
-  def post
-  end
-
+  
   def tweet
+    tweets = Tweet.all
+    respond_to do |format|
+      format.html
+      format.json { render json: tweets }
+    end
   end
 end

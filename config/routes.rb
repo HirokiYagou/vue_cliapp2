728958e0ee7e8e4e0199b54f32295a3bf7f89dee
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-20 19:05:09 +0900

    deviseのルーティングを元の設定に戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
-  devise_scope :user do
-    get 'login' => 'devise/sessions#new', as: :new_user_session
-    post 'login' => 'devise/sessions#create', as: :user_session
-    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
-    get 'signup' => 'devise/registrations#new', as: :new_user_registration
-    post 'signup' => 'devise/registrations#create', as: :user_registration
-    get 'password' => 'devise/passwords#new', as: :new_user_password
-    post 'password' => 'devise/passwords#create', as: :user_password
-  end
+  devise_for :users
   root to: 'exercises#index'
   resources :exercises, only: :index do
     collection do
